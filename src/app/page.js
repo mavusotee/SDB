6f63de9fc8806bb195622bfb6bd8d4b4d46b7d42
Thokozani Mavuso <158532236+mavusotee@users.noreply.github.com>
2024-04-30 19:25:13 +0200

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -8,6 +8,7 @@ import Hero from "./components/Hero";
 import Section1 from "./components/Section1";
 import Section2 from "./components/Section2";
 import Section3 from "./components/Section3";
+import ErrorBoundary from "./ErrorBoundary";
 
 
 
